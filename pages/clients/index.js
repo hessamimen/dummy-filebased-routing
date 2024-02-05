4b3a7a82ad47e3f6348acb87817ee3e01af2a00e
Hessam <hessam.imen@gmail.com>
2024-02-06 03:30:42 +1100

    feat: Navigate to dynamic routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,4 +1,21 @@
+import Link from "next/link";
+
 function ClientsPage() {
-  return <div>ClientsPage</div>;
+  const clients = [
+    { id: "hessam", name: "Hessam" },
+    { id: "max", name: "Maximilian" },
+  ];
+  return (
+    <div>
+      <h1>ClientsPage</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 export default ClientsPage;
