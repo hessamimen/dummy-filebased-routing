dab0134d2e517a054cf26619ba6db1d56d4991b7
Hessam <hessam.imen@gmail.com>
2024-02-06 03:13:33 +1100

    Add catch-all routes

---
+++ pages/blog/[...slug].js
@@ -0,0 +1,8 @@
+import { useRouter } from "next/router";
+
+function BlogPostsPage() {
+  const router = useRouter();
+  console.log(router.query);
+  return <div>BlogPostsPage</div>;
+}
+export default BlogPostsPage;
--- pages/clients/[id]/[clientprojectid].js
+++ pages/clients/[id]/[clientprojectid].js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 function SelectedClientProjectPage() {
   const router = useRouter();
 
-  console.log(router.query.id);
+  console.log(router.query);
   return <div>SelectedClientProjectPage</div>;
 }
 export default SelectedClientProjectPage;
