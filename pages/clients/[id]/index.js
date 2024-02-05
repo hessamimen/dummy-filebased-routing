d70eebcc5217c829ba70360320ca9246267b9c44
Hessam <hessam.imen@gmail.com>
2024-02-06 04:05:10 +1100

    feat: Navigate programatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,4 +1,24 @@
+import { useRouter } from "next/router";
+
 function ClientProjectPage() {
-  return <div>ClientProjectPage</div>;
+  const router = useRouter();
+
+  console.log(router.query);
+
+  function loadProjectHandler() {
+    router.push({
+      pathname: `/clients/[id]/[clientprojectid]`,
+      query: {
+        id: router.query.id,
+        clientprojectid: "projecta",
+      },
+    });
+  }
+  return (
+    <div>
+      <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
+    </div>
+  );
 }
 export default ClientProjectPage;
